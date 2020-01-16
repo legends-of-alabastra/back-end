cfcd3ed7bb7a75c6394b1f9b570dfed2658dc322
Miguel <diaz4674@gmail.com>
2020-01-15 23:02:30 -0800

    removed file

--- MasterCaptainFile/MasterCaptainFile/settings.py
+++ MasterCaptainFile/MasterCaptainFile/settings.py
@@ -124,5 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')**,** <- that comma
+)
 django_heroku.settings(locals())
