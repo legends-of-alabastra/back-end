d0533ef3061a02cec6efbc4dfc3b4c5b166fc263
Christian-Ford <fjord.christian@gmail.com>
2020-01-21 21:26:42 -0600

    Migrated items into database

--- MasterCaptainFile/MasterCaptainFile/settings.py
+++ MasterCaptainFile/MasterCaptainFile/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'users.apps.UsersConfig',
+    'pirateAdventure.apps.PirateadventureConfig'
 ]
 
 MIDDLEWARE = [
