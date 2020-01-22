f2f53c03ba5e349b4fcffa8622be3b43d5553d37
Christian-Ford <fjord.christian@gmail.com>
2020-01-22 11:33:41 -0600

    Fixed Player Class

--- MasterCaptainFile/pirateAdventure/models.py
+++ MasterCaptainFile/pirateAdventure/models.py
@@ -66,45 +66,13 @@ class Weapon(Item):
 # PLAYER CLASS'S
 #########################################################
 #########################################################
-# class Player(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     currentRoom = models.IntegerField(default = 0)
-#     uuid = models.UUIDField(default = uuid.uuid4, unique=true)
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    currentRoom = models.IntegerField(default = 0)
+    uuid = models.UUIDField(default = uuid.uuid4, unique=true)
 
-#     def initialize(self):
-#         if self.currentRoom == 0:
-#             self.currentRoom = Room.objects.first().id
-#             self.save()
+    def initialize(self):
+        if self.currentRoom == 0:
+            self.currentRoom = Room.objects.first().id
+            self.save()
     
-=======
-# class Player(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     currentRoom = models.IntegerField(default = 0)
-#     uuid = models.UUIDField(default = uuid.uuid4, unique=true)
-
-#     def initialize(self):
-#         if self.currentRoom == 0:
-#             self.currentRoom = Room.objects.first().id
-#             self.save()
-    
-
-#     def room(self):
-#         try:
-#             return Room.objects.get(id = self.currentRoom)
-#         except Room.DoesNotExist:
-#             self.initialize()
-#             return self.room()
-
-
-# @receiver(post_save, sender = User)
-
-# def create_user_player(sender, instance, created, **kwargs):
-#     if created:
-#         Player.objects.create(user = instance)
-#         Token.objects.create(user = instance)
-
-
-# @receiver(post_save, sender = User)
-
-# def save_user_player(sender, instance, **kwargs):
-#     instance.player.save()
