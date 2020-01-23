48efc333f1996e990f9bc7910f4b0399f4f084ce
Mychal Hall <hallmychal@gmail.com>
2020-01-22 22:42:42 -0600

    added game text for empty rooms, rooms with items, shopkeepers,land, towns, player combat, enemy combat, logout, login, and emotes. rooms.py

---
+++ MasterCaptainFile/pirateAdventure/rooms.py
@@ -0,0 +1,146 @@
+
+# Login message
+
+# "Welcome to Legends of Alabastra! We hope you enjoy your time on the open sea! REMEMBER: YOU CAN NOT KILL THE KRAAKEN! If you see one. Sail AWAY."
+
+# 10 'Entered a new sea descriptions'
+
+# 1 -- "All around you, the water begins to shift and shimmer. Going from a sea blue, to a light green. You have now entered the {SEA NAME}."
+# 2 -- "Frost begins creeping up the side of your ship and there is a chill in the air. Welcome to {SEA NAME}."
+# 3 -- "The sky begins filling with clouds. Thunder rumbles and storms lie on the horizon. The is the notorious {SEA NAME}."
+# 4 -- "Legends from your youth come flooding back. Could it be? The {SEA NAME}?"
+# 5 -- "Many brave captains have brought their ships here, to the {SEA NAME} to find glory. Will you succeed where they did not?"
+# 6 -- "A flock of gulls soars overhead. No big deal. Except now you're in {SEA NAME}."
+# 7 -- "A crewmember must be asleep at the helm. Why else would you have strayed into {SEA NAME}?"
+# 8 -- "Dark thoughts fill you mind. Do you have what it takes to survive in {SEA NAME}?"
+# 9 -- "{SEA NAME} is to Legends of Alabastra as Goldshire is to World of Warcraft."
+# 10 -- "The water begins darkening until it is almost pitch black. The vast expanse of {SEA NAME} lies ahead."
+
+# 24 'EMPTY Room descriptions'
+
+# 1 -- "The ocean spray hits your face, reminding you that it is INDEED great to be a pirate."
+# 2 -- "Here's some water. There's some water. Everywhere is just... water."
+# 3 -- "Is that another player!? Nah. Just lovely ocean."
+# 4 -- "Yo ho ho! A pirate's life for you. Nothing but water here."
+# 5 -- "How much do you pay your sailors to run this ol' ship? Water. That's all there is here."
+# 6 -- "Gazing out at the ocean. You try to convince yourself that heading out to sea was the right choice. Nothing but undrinkable sea water surrounds you."
+# 7 -- "Here's the thing about ocean. It's usually all the same. Nothing to "sea" here. lolz."
+# 8 -- "Quick question? Did you become a pirate for the awesome shanties we get to sing? I did. Nothing but water here. Sorry."
+# 9 -- "You getting thirsty? Empty ocean surrounds you. "
+# 10 -- "Finally! Nothing is here."
+# 11 -- "Ahoy matey. You've discovered no awesome treasure here. Nothing but water."
+# 12 -- "Oh, I have seen wonders you never have dreamed, and taken my fair share I must saaayyy. Nice song. Nothing here."
+# 13 -- "The open air. The smell of the sea. That's all there is here."
+# 14 -- "Shouts erupt from the below deck. Followed by laughter. Someone fell down. Nothing but water surrounds you."
+# 15 -- "Nothing like spending a day sailing the ocean, looking for things. Nice work on that. Nothing but water 'round 'ere."
+# 16 -- "Piracy is a way of life. Water. That's all there is here."
+# 17 -- "The rum is definitely NOT gone... yet. Empty ocean surrounds you."
+# 18 -- "So you thought this was going to be something cool?! Like a kraaken or a ghost ship!!! Is that what you WANT?!?! Well fine then. Be patient. Nothing but water here."
+# 19 -- "A shiver of sharks is trailing you and you could've sworn you just saw... lasers strapped to their heads... odd. Nothing but water here."
+# 20 -- "Well, you found some ocean. Nice."
+# 21 -- "Wind in the sails, water all around. Nothing here."
+# 22 -- "As your crew starts to swab the deck you notice that you are surrounded by empy ocean."
+# 23 -- "Maybe there will be treasure in the next "room". But it's not here."
+# 24 -- "There's been a legendary treasure found! But not by you. Nothing but ocean surrounds you."
+
+# 24 'Room descriptions'
+
+# 1 -- "As the waves gently rock your ship, you notice: {ITEM}"
+# 2 -- "Floating alongside the ship is: {ITEM}" 
+# 3 -- "A shiver of sharks is here, circling: {ITEM}" 
+# 4 -- "Below you, the remains of a sunken ship. Floating around this area is: {ITEM}" 
+# 5 -- "'Things ho!' Comes the shout from the crow's nest. Looking around you see: {ITEM}" 
+# 6 -- "Empty ocean is everywhere but here. Around you is: {ITEM}" 
+# 7 -- "Well would you look at that? Gently bumping the side of your ship is: {ITEM}" 
+# 8 -- "Sure would be nice to find something. Oh, wait! Look at this: {ITEM}" 
+# 9 -- "Gulls are perched on something in the water. Looking closer you see: {ITEM}" 
+# 10 -- "As the sun sinks towards the horizon you see: {ITEM}" 
+# 11 -- "You finish off your 200th mead for the day. Nice. Running to the side of the ship, you see: {ITEM}" 
+# 12 -- "Another boring area of the sea. Except that it's NOT! There are things here: {ITEM}" 
+# 13 -- "Falling asleep on a big pile of gold you notice: {ITEM}" 
+# 14 -- "Pirates always have a way of finding things. Maybe you do to. Around you is: {ITEM}" 
+# 15 -- "You have the sudden urge to take something that isn't yours. Lucky for you there is: {ITEM} here." 
+# 16 -- "You're just about to start making people walk the plank until you see: {ITEM} here." 
+# 17 -- "Wait! Is that treasure? There is: {ITEM} here." 
+# 18 -- "Your crew just finished eating 99 bits of leather. There is: {ITEM} here." 
+# 19 -- "You notice a skeleton float by. Around it is: {ITEM}" 
+# 20 -- "There are no shortcuts in life. Unless you are a pirate. There are: {ITEM} here." 
+# 21 -- "Argh! There is: {ITEM} here." 
+# 22 -- "In the distance you see a mermaid perched on a rock. Much closer you see: {ITEM}." 
+# 23 -- "The rum is almost gone. Also, there is: {ITEM} here" 
+# 24 -- "Pirate stuff floats around your ship: {ITEM}" 
+
+# 10 'Town Descriptions'
+
+# 1 -- "The smell of rotting fish permeates the air around you! Welcome to town!"
+# 2 -- "You and your crew shamble into town. Ready to raise a ruckus... or something."
+# 3 -- "As you enter the town, people don't care. Can you imagine NOT caring about seeing a PIRATE walking into town... wow."
+# 4 -- "Walking into town a fight breaks out... that's just how things go... wait for it... you win the fight. Silly townspeople."
+# 5 -- "As you walk into town, you notice that all the people stop and stare at you... you left your pants on the ship."
+# 6 -- "As you and your crew make your way into town, you suddenly feel very thirsty. Time for rum."
+# 7 -- "A dingy assortment of shacks make up the 'town'. Color you not impressed."
+# 8 -- "Rum, beer, quests, and mead. These are the things that a pirate needs... also towns. You've entered one."
+# 9 -- "As you make your way into town, everybody starts running away. Nice work, scary pirate."
+# 10 -- "You enter a town. Time to make some gold."
+
+# 2 'Land Descriptions'
+
+# 1 -- "Trees are here. Cool beans."
+# 2 -- "Trees, brush, shrubs... it's land. Pirates don't like land."
+
+# 5 'Combat Miss Descriptions' (They miss you)
+
+# 1 -- "Chain shot soars overhead, barely missing your mainsail!"
+# 2 -- "Skadoooosh. Cannonballs are hitting all around you."
+# 3 -- "Pew pew pew. They missed. Also their cannons sound WEAK!"
+# 4 -- "The enemy launches a full broadside volley directly at you... they MISSED every shot. They are bad at this."
+# 5 -- "Thunk. I think they hit you with a rock? I don't know. You took no damage. Carry on."
+
+# 5 'Combat Hit Descriptions' (They hit you)
+
+# 1 -- "The sound of splintering wood fills the air around you as your ship is rocked by a cannonball."
+# 2 -- "Trickshot! They just hit one of your cannons with a cannonball. Ouch!"
+# 3 -- "As cannon fire fills the air. Shattering glass and the breaking of wood pierces the air. They hit the captain's cabin. Rude."
+# 4 -- "They hit your battleship. With a cannonball. Because you are pirates."
+# 5 -- "Yikes! A cannonball rips through the forward sails!"
+
+# 5 'Combat Miss Descriptions' (You miss them)
+
+# 1 -- "Whiffed it. Try again. This time... aim at the enemy ship."
+# 2 -- "I think they might be hacking. You missed them."
+# 3 -- "You pull alongside the enemy ship and unleash the full might of a broadside volley!!! Unfortunately you missed them completely."
+# 4 -- "Miss! Slow clap. Did you hire this crew because of their skill or just their looks?"
+# 5 -- "You missed... you sure you know how to do this?"
+
+# 5 'Combat Hit Descriptions' (You hit them)
+
+# 1 -- "You smile grimly as you see your cannonballs tear into the enemy ship."
+# 2 -- "Ha ha! Pirates - 1... other pirates - 0! You hit them!"
+# 3 -- "360 noScope!!! Your cannonball hits the guy steering the other ship!"
+# 4 -- "Pirate skillz. Another hit!"
+# 5 -- "That last volley tore into the enemy right at the waterline! Maybe they'll sink now."
+
+# 5 'Shopkeeper dialogue'
+
+# 1 -- "Welcome to the shop! You wanna trade?"
+# 2 -- "If you've got the goods! I've got the gold!"
+# 3 -- "Welcome to my little store. I mostly deal in antiques... but anything will do!"
+# 4 -- "Well it's like they say, 'You never know WHAT'S gonna walk through that door.'"
+# 5 -- "Hey, I'm Ronnie the shopkeeper, although once I did have dreams of being a pirate... Anyways, you gots the goods?"
+
+# Logout Message
+
+# "Fare thee well Cap'n. The crew'll keep the ship while you're away."
+
+# Emotes
+
+# /sing -- "Rum, beer, quests, and mead
+# These are things that a pirate needs
+# Raise the flag, and let's set sail
+# Under the sign of the storm of ale"
+
+# /dance -- "You dance a tasty jig. Yo ho!"
+
+# /curse -- "Pirates and curses... don't go messing with that."
+
+# /joke -- "Why did ye chicken cross thee road? To bite ye in the mainsail, arrrgh!"
